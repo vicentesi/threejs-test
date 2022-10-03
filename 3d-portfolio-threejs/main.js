bc892a09af49e7507fe172b01ee673ed3d9d4a83
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2022-10-03 00:41:50 -0300

    wip

--- 3d-portfolio-threejs/main.js
+++ 3d-portfolio-threejs/main.js
@@ -7,6 +7,7 @@ const COLORS = {
   black: 0x000000,
   torus: 0x8AEB00,
   sphere: 0x5000FF,
+  platform: 0x202124
 }
 
 const scene = new THREE.Scene();
@@ -23,12 +24,12 @@ camera.position.set(30, 15, 30);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 50);
 const material = new THREE.MeshStandardMaterial({ color: COLORS.torus });
 const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 
-const geometry1 = new THREE.SphereGeometry(10,30,15);
-const material1 = new THREE.MeshStandardMaterial({ color: COLORS.sphere});
+const geometry1 = new THREE.SphereGeometry(10, 30, 15);
+const material1 = new THREE.MeshStandardMaterial({ color: COLORS.sphere });
 const sphere = new THREE.Mesh(geometry1, material1);
-
-scene.add(torus, sphere);
+scene.add(sphere);
 
 // LIGHT
 const pointLight = new THREE.PointLight(COLORS.white);
@@ -36,10 +37,23 @@ const ambientLight = new THREE.AmbientLight(COLORS.white);
 pointLight.position.set(15, 15, 5);
 scene.add(pointLight, ambientLight);
 
+// FOG
+scene.fog = new THREE.Fog(
+  0xFFFFFF,
+  camera.position.z + 5,
+  camera.position.z + 200
+);
+
+// PLATFORM
+const platformG = new THREE.CylinderGeometry(170, 85.2, 1, 85);
+const platformM = new THREE.MeshLambertMaterial({ color: COLORS.platform });
+const platformMesh = new THREE.Mesh(platformG, platformM);
+scene.add(platformMesh);
+
 // HELPERS
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 30, 0x8eadff);
-scene.add(lightHelper, gridHelper);
+// scene.add(lightHelper, gridHelper);
 
 // CONTROL
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -68,8 +82,8 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-// const spaceTexture = new THREE.TextureLoader().load('./img/space.png');
-// scene.background = spaceTexture;
+const spaceTexture = new THREE.TextureLoader().load('./img/space.png');
+scene.background = spaceTexture;
 
 Array(1500).fill().forEach(addStar);
 
